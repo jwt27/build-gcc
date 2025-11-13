de793f597daffc96888a8ac3ff16cd78eae69614
J.W. Jagersma <jwjagersma@gmail.com>
2025-11-13 17:15:15 +0100

    patch djasm to make it compile with gcc 15
    Also applied to djgpp-cvs.
    fixes: #51 (but better)

--- build-djgpp.sh
+++ build-djgpp.sh
@@ -112,7 +112,7 @@ if [ ! -z ${DJGPP_VERSION} ]; then
   cd src
   unset COMSPEC
   sed -i "50cCROSS_PREFIX = ${TARGET}-" makefile.def
-  sed -i "61cGCC = ${CC} -g -O2 -std=gnu17 ${CFLAGS}" makefile.def
+  sed -i "61cGCC = ${CC} -g -O2 ${CFLAGS}" makefile.def
   if [ ! -z ${GCC_VERSION} ] || [ ! "`cat configure-options 2> /dev/null`" == "${TARGET}:${DST}:${CFLAGS_FOR_TARGET}" ]; then
     echo "Cleaning djgpp source tree..."
     ${MAKE_J} clean > /dev/null 2>&1
