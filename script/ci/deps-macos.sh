9a41eab58311b0e5459757f2f0a98d2733e37a26
J.W. Jagersma <jwjagersma@gmail.com>
2026-04-26 22:17:29 +0200

    CI: fix missing backslash

--- script/ci/deps-macos.sh
+++ script/ci/deps-macos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -xe
 
 brew update || :
 brew install --formula \
--- script/ci/deps-ubuntu.sh
+++ script/ci/deps-ubuntu.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-set -e
+set -xe
 
 GCC_VERSION=13
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test || :
 sudo apt-get update || :
-sudo apt-get install
+sudo apt-get install \
   bison flex curl make texinfo zlib1g-dev tar bzip2 gzip xz-utils unzip \
   dos2unix libtool-bin gcc-$GCC_VERSION g++-$GCC_VERSION cmake help2man \
   python3-dev nasm libslang2-dev ccache
